eced8c2adbfa5ae8b3423c06dd77f1fa03f0bddc
david lopes <devlopes10@gmail.com>
2025-02-06 18:57:20 -0300

    fim

---
+++ beck-end-java-2/minha classe/src/BeckEndJava/SintaxJava/TiposVariaveis.java
@@ -0,0 +1,20 @@
+package BeckEndJava.SintaxJava;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        /*
+
+         * VALORES DE CADA VARIAVEL
+         * ______________________________________________________________________________
+         * TIPO    | MEMORIA       |VALR/MIN                   |VALOR/MAX                |
+         * Byte    | 1 byte        |-128                       |127                      |
+         * Short   | 2 byte        |-32.768                    |32.768                   |
+         * Int     | 4 byte        |-2.147.483.648             |2.147.483.648            |
+         * Long    | 8 byte        |-9.223.372.036.854.775.808 |9.223.372.036.854.775.808|
+         * Float   | 4 byte        |-3,4028E + 38              |3,4028E + 38             |
+         * Double  | 8 byte        |-1,7976E + 308             |1,7976E + 308            |
+         */
+        
+    }
+    
+}
